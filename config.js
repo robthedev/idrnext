b174ec708360885eb4752d59df61e0e33a7d6674
R Will <kemet@Rs-MacBook-Pro.attlocal.net>
2022-12-26 16:33:34 -0600

    adfsasfsdf

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000/api' // development api
-    : 'http://localhost:3000/api'; // production api
+    : 'https://idrtemp-robthewebdev.vercel.app/api'; // production api
 
 export {
     apiUrl
